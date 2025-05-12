ef740a2bb5fdf7f3740b558b388265e6e233e6f7
Milosz Komarnicki <milosz.kom@onet.pl>
2025-05-12 10:21:46 +0200

    zmiana 5

--- github-learn/app/page.tsx
+++ github-learn/app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org → lol
         </a>
       </footer>
     </div>
