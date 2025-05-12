12ade3b5e21948d2305a822e3cc1a1ba0164bb4b
Milosz Komarnicki <milosz.kom@onet.pl>
2025-05-12 09:19:43 +0200

    some changes from milosz-branch

--- github-learn/app/page.tsx
+++ github-learn/app/page.tsx
@@ -24,6 +24,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Example change from milosz branch
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
