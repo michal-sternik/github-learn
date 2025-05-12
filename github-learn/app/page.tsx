1d88dee65ce332a302639ecfecbc53d9c6df3629
Milosz Komarnicki <milosz.kom@onet.pl>
2025-05-12 10:20:16 +0200

    zmiana 1

--- github-learn/app/page.tsx
+++ github-learn/app/page.tsx
@@ -28,9 +28,6 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li className="tracking-[-.01em]">
-            Example change from milosz branch
-          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -48,14 +45,6 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
           </a>
         </div>
       </main>
