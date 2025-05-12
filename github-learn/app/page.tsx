af0dfd5f4efc5c5b34b84dcb059a3f7fe29e9d21
michal-ster <michal.sternik01@gmail.com>
2025-05-12 10:20:34 +0200

    # This is a combination of 3 commits. # This is the 1st commit message:
    michal-c-1
    
    # This is the commit message #2:
    
    michal-c-3
    
    # This is the commit message #3:
    
    michal-c-4

--- github-learn/app/page.tsx
+++ github-learn/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div> Mati changes </div>
+        <div> michal changes </div>
 
         <HelloMichal />
-
+        <div>michal</div>
         <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={90}
-          height={39}
+          height={35}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -29,7 +29,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
           <li className="tracking-[-.01em]">
-            Example change from milosz branch
+            Example change from milosz and michal branch
           </li>
         </ol>
 
